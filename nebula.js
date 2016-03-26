b0fb4c97ef102a282c0499fd8fb67ca436a217bb
John Smith <qjqjqzy@gmail.com>
2016-03-26 15:51:56 +0800

    Loader

--- nebula.js
+++ nebula.js
@@ -4,97 +4,11 @@ var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
 var Loader = {
-    jslist : [],
-    coreJS : [
-        "core/Class.js",
-        "core/Director.js",
-        "core/Texture.js",
-        "core/Common.js",
-        "core/Scene.js",
-        "core/Sprite.js"
-    ],
-    extJS : [
-        "ext/Button.js",
-        "ext/Label.js"
-    ],
-    textureCount : 0,
-    tc : 0,
 
-    _loadScript : function (src, callback) {
-        var head = document.getElementsByTagName('head')[0];
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = src;
-        if (callback) {
-            script.onreadystatechange = function () {
-                if (this.readyState == 'loaded') callback();
-            }
-            script.onload = callback;
-        }
-        head.appendChild(script);
-    },
-    _loadImage : function (name) {
-        var img = new Image();
-        img.onload = function() {
-            Loader.display("Texture " + name);
-            Texture.addTexture(name, img);
-            Loader.textureCount++;
-            if (Loader.textureCount == Loader.tc) {
-                Loader.loadSrc();
-            }
-        }
-        img.src = Resource.texture[name];
-    },
+    sourcefile : "resource.json",
+    //Requests Queue
+    RQ : [],
 
-    loadCore : function () {
-        if (this.coreJS.length > 0) {
-            var src = this.coreJS.shift();
-            Loader._loadScript(
-                src,
-                function () {
-                    Loader.display("Script " + src);
-                    Loader.loadCore();
-                }
-            )
-        } else {
-            this.loadExt();
-        }
-    },
-
-    loadExt : function () {
-        if (this.extJS.length > 0) {
-            var src = this.extJS.shift();
-            Loader._loadScript(
-                src,
-                function () {
-                    Loader.display("Script " + src);
-                    Loader.loadCore();
-                }
-            )
-        } else {
-            this.loadTexture();
-        }
-    },
-    loadTexture : function () {
-        var i;
-        for (i in Resource.texture) {
-            this._loadImage(i);
-        }
-    },
-    loadSrc : function() {
-        if (this.jslist.length > 0) {
-            var src = this.jslist.shift();
-            Loader._loadScript(
-                src,
-                function () {
-                    Loader.display("Script " + src);
-                    Loader.loadSrc();
-                }
-            )
-        } else {
-            this.onReady();
-        }
-    },
     display : function (src) {
         context.fillStyle = "white";
         context.fillRect(0, 0, canvas.width, canvas.height);
@@ -106,24 +20,79 @@ var Loader = {
         //test
         // for(var t = Date.now();Date.now() - t <= 1000;);
     },
-    request : function () {
-        this.jslist = Resource.src;
-        for (i in Resource.texture) {
-            this.tc += 1;
+
+    _loadData : function(path, callback) {
+        console.log("load " + path);
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.type = "text/javascript";
+        script.src = path;
+        if (callback) {
+            // script.onreadystatechange = function () {
+            //     if (this.readyState == 'loaded') callback();
+            // }
+            this.display("Script " + path);
+            script.onload = callback;
         }
-        this.loadCore();
+        head.appendChild(script);
+    },
+    _loadImage : function (name, path, callback) {
+        console.log("load " + path);
+        var img = new Image();
+        img.onload = function() {
+            Loader.display("Texture " + name);
+            Texture.addTexture(name, img);
+            if (callback) {
+                callback();
+            }
+        }
+        img.src = path;
+    },
+    _init : function() {
+        if (typeof(Resource) != "undefined" && Resource) {
+            for (var i in Resource){
+                for (var j in Resource[i]) {
+                    if (Array.isArray(Resource[i])){
+                        this.RQ.push({t:i, v:Resource[i][j]});
+                    } else {
+                        this.RQ.push({t:i, k: j, v: Resource[i][j]});
+                    }
+                }
+            }
+        } else {
+            throw new URIError("The resource is not accessible.");
+        }
+        this._request();
+    },
+    _request : function() {
+        if (this.RQ.length > 0) {
+            var source = this.RQ.shift();
+            if (source.t == "src") {
+                this._loadData(source.v, this._request.bind(this));
+            } else if (source.t == "texture") {
+                this._loadImage(source.k, source.v, this._request.bind(this));
+            } else {
+                // other data
+            }
+        } 
+        // complete
+        else {
+            if (typeof(Nebula) == "undefined") {
+                throw new Error("Nebula load error!");
+            }
+
+            Nebula.Director.init(canvas, context);
+
+            document.onkeydown = Nebula.Director.onkeydown;
+            document.onkeyup = Nebula.Director.onkeyup;
+            document.onkeypress = Nebula.Director.onkeypress;
+
+            Nebula.Director.runScene(new MainScene());
+        }
+    },
+    loading : function(){
+        //init resource 
+        this._loadData(this.sourcefile, this._init.bind(this));
     }
-}
-
-Loader.onReady = function () {
-
-    Nebula.Director.init(canvas, context);
-
-    document.onkeydown = Nebula.Director.onkeydown;
-    document.onkeyup = Nebula.Director.onkeyup;
-    document.onkeypress = Nebula.Director.onkeypress;
-
-    Nebula.Director.runScene(new MainScene());
-}
-
-Loader.request();
+};
+Loader.loading();
